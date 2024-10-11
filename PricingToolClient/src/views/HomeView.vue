42cd3155057ec4b919d7e259e72755c221cbc550
Stefan Chernev <schernev@gmail.com>
2024-10-11 14:26:10 +0300

    Front-end: initial commit, empty vite vue.js project

---
+++ PricingToolClient/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template>
+    <div class="about">
+        <h1>
+            This sample app is created by Stefan Chernev. <br />
+            The app retrieves real-time currency prices from the backend using Web Sockets and displays them in a component
+        </h1>
+    </div>
+</template>
